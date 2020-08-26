c4752bb06317740efce8230aeb8d1a1c18baa837
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-26 22:13:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -14,30 +14,29 @@ board = [" "," "," "," "," "," "," "," "," "]
  def display_board(board)
  puts "#{board[0]}  |#{board[1]} |#{board[2]}"
  puts "-----------"
- puts " #{board[3]} |#{board[4]} |#{board[5]}"
+ puts "#{board[3]}  |#{board[4]} |#{board[5]}"
  puts "-----------"
  puts "#{board[6]}  |#{board[7]} |#{board[8]}"
 end
 display_board(board)
-board[5] = X
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(board)
+#board[5] = X
+#board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+#display_board(board)
 
-board[0] = O
-board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
+#board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+#display_board(board)
 
 
 #board[0] = "O"
 #board[4] = "X"
-board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
-display_board(board)
+#board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+#display_board(board)
 
-board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
-display_board(board)
+#board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+#display_board(board)
 
-board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
-display_board(board)
+#board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+#display_board(board)
 
-board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
-display_board(board)
+#board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+#display_board(board)
