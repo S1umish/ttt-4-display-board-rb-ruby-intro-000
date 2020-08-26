f839da1291964e012bc103ac9abf34413bf3ddb6
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-26 22:29:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,11 +12,11 @@
 
 board = [" "," "," "," "," "," "," "," "," "]
  def display_board(board)
- puts "#{board[0]}  |#{board[1]}  |#{board[2]}  "
+ puts " #{board[0]}  |#{board[1]}  |#{board[2]}  "
  puts "-----------"
- puts "#{board[3]}  | #{board[4]} |#{board[5]}  "
+ puts " #{board[3]}  | #{board[4]} |#{board[5]}  "
  puts "-----------"
- puts "#{board[6]}  |#{board[7]}  |#{board[8]}  "
+ puts " #{board[6]}  |#{board[7]}  |#{board[8]}  "
 end
 display_board(board)
 #board[5] = X
@@ -24,8 +24,8 @@ board = [" ", " ", " ", " ", "X" , " ", " ", " ", " "]
 display_board(board)
 
 
-#board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
-#display_board(board)
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
 
 
 #board[0] = "O"
