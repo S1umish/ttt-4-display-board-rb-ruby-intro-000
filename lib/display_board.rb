747f990b8fc29f1c83695aef9eb7e869230711f1
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-26 22:15:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,24 +1,24 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-display_board(board)
-
 #board = [" "," "," "," "," "," "," "," "," "]
- #def display_board(board)
- #puts "#{board[0]}  |#{board[1]} |#{board[2]}"
- #puts "-----------"
- #puts "#{board[3]}  |#{board[4]} |#{board[5]}"
- #puts "-----------"
- #puts "#{board[6]}  |#{board[7]} |#{board[8]}"
+#def display_board(board)
+  #puts "   |   |   "
+  #puts "-----------"
+  #puts "   |   |   "
+  #puts "-----------"
+  #puts "   |   |   "
 #end
 #display_board(board)
+
+board = [" "," "," "," "," "," "," "," "," "]
+ def display_board(board)
+ puts "#{board[0]}  |#{board[1]} |#{board[2]}"
+ puts "-----------"
+ puts "#{board[3]}  |#{board[4]} |#{board[5]}"
+ puts "-----------"
+ puts "#{board[6]}  |#{board[7]} |#{board[8]}"
+end
+display_board(board)
 #board[5] = X
 #board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 #display_board(board)
