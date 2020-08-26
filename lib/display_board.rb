511e7f2e9d54d4bdf0607ea14c0100f1510f9e24
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-26 21:48:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,16 +12,16 @@ display_board(board)
 
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "#{board[0]}  |#{board[1]} |#{board[2]}"
   puts "-----------"
-  puts " #{board[3]}|#{board[4]}|#{board[5]}"
+  puts " #{board[3]} |#{board[4]} |#{board[5]}"
   puts "-----------"
-  puts "#{board[6]}|#{board[7]}|#{board[8]}"
+  puts "#{board[6]}  |#{board[7]} |#{board[8]}"
 end
 display_board(board)
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 board[5]= X
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board)
 end
 
